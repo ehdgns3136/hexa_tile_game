468bb8c9b1b50b148885f4b9dd08356c5b8c9521
unknown <ehdgns3136@naver.com>
2019-04-21 05:58:23 +0900

    remove building concept, instead, add unit type to unit class

--- hexaTBS/Assets/Resources/Scripts/Unit.cs
+++ hexaTBS/Assets/Resources/Scripts/Unit.cs
@@ -7,14 +7,23 @@ namespace Resources.Scripts
         private int defense;
         private int movement;
         private int sight;
+        private UnitType type;
+        
+        public enum UnitType
+        {
+            NONE,
+            HUMAN,
+            BUILDING
+        }
 
-        public Unit(int hp, int offense, int defense, int movement, int sight)
+        public Unit(int hp, int offense, int defense, int movement, int sight, UnitType type)
         {
             this.hp = hp;
             this.offense = offense;
             this.defense = defense;
             this.movement = movement;
             this.sight = sight;
+            this.type = type;
         }
     }
 }
